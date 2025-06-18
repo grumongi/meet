b5290ce345f65c536bf4249faec3023b6ff7860e
grumongi <gustaverumongi@gmail.com>
2025-06-18 16:33:28 -0700

    task 4-9 Progressive Web Apps

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as atatus from 'atatus-spa';
+//import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-atatus.config('85ed7d3f877c41e589b754824c230be9').install();
+//atatus.config('85ed7d3f877c41e589b754824c230be9').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+serviceWorkerRegistration.register();
+
 //atatus.notify(new Error('Test Atatus Setup'));
